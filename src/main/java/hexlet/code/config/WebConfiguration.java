90ef18f80843bfe042321ced0648800931707a9d
DenisJD <t.denisjd@gmail.com>
2023-02-02 21:36:32 +0300

    move passwordEncoder to AppApplication

--- src/main/java/hexlet/code/config/WebConfiguration.java
+++ src/main/java/hexlet/code/config/WebConfiguration.java
@@ -1,11 +1,8 @@
 package hexlet.code.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
